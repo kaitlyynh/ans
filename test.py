dc72f699a8c41662944b5436ecff4a49067977fc
Kaitlyn Huynh <87906190+kaitlyynh@users.noreply.github.com>
2025-04-19 12:19:07 -0400

    Update test.py

--- test.py
+++ test.py
@@ -3,32 +3,41 @@ def question1():
     my_tuple = (0, 1, 2) #a
     my_tuple[2] = 4
     print(my_tuple[2])
+    # ERROR
 
     my_str = "school" #b
     print(my_str[5: :-2]) 
+    # "loc"
 
     cool_stuff = "Math is cool" #c
     print(cool_stuff.split())
+    # ["Math", "is", "cool"]
 
     lst = [0, 1, 2, 3, 4] #d
     lst.insert(2, 3)
     print(lst)
+    #[0, 1, 3, 2, 3, 4]
 
     lst = [0, 1, 2, 3, 4] #e
     print(lst.pop(2))  
+    # 2
 
     lst = [0, 1, 2, 3, 4] #f
     lst.pop()
     lst.append(5)
     print(lst)
+    # [0, 1, 2, 3, 5]
 
     dict = {"a": 0, "b": 1} #g
     dict["c"] = 2
     print(dict)
+    #{“a”: 0, “b”: 1, “c”: 2}
+
 
     lst = [1, 2, 3, 4, 5, 6] #h
     new_lst = lst.reverse()
     print(new_lst[1]) 
+    #ERROR
 
 # question 2
 def display_triangle(symbol, n):
@@ -58,7 +67,81 @@ def question3():
     y = 10
     func2(y)
 
-question3()
+"""Output:
+25 9 1
+4
+1
+"""
+
+# question3()
+
+
+def question4():
+    try:
+        f = open('test_file.txt', 'r+')
+    except FileNotFoundError as err:
+        print('look here: \t',err)
+        f = open('solid_file.txt','r+')
+    finally:
+        ct = 0
+        for each in f.read():
+            if each.isalpha():
+                ct+=1
+        f.write(f'ct: {ct}')
+        f.close()
+
+def question5():
+    # does this code work? if not, what error does it give?
+    s = "the 167 20 lazy 210 brown 78 87 2 fox 245 123"
+    lst = s.split()
+    count = 0
+    for elem in lst:
+        count += int(elem)
+    print(count)
+
+
+# question 6
+def get_player_dict():
+    player_dict = {}
+    num_of_players = int(input("Number of players: "))
+    for i in range(num_of_players):
+            player_stats = get_player_stats() 
+            # add player to dictionary with their stats
+            # ... 
+            player_dict[player_stats[0]] = (player_stats[1], player_stats[2])
+    return player_dict
+
+def get_player_stats(): # PART A)
+    name = input("Player name: ")
+    games_played = int(input("Games played: "))
+    hits = int(input("Number of hits: "))
+    pa = int(input("Number of plate appearances: "))
+    bat_avg = hits / pa
+    return (name, games_played, bat_avg)
+
+"""
+PART B)
+player_dict[player_stats[0]] = (player_stats[1], player_stats[2]) 
+"""
+import copy
+def question7():
+    lst = [1,2,[3,4,5]]
+    lst2 = lst[1:]
+    lst3 = copy.deepcopy(lst)
+    lst[2][2] = 0
+    print(f"lst:{lst}\n lst2:{lst2}\n lst3:{lst3}")
+
+
+
+
+
+
+    
+
+
+
+
+
 
 
 
